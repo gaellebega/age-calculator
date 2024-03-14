f4d686d1c363168660e9a397aef61661e3d4fd04
gaellebega <gaellebega@gmail.com>
2024-03-14 11:59:46 +0200

    corrections

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ const theOutput = document.getElementById("date");
 theOutput.innerHTML = "...your age will be calculated soon";
 
 function displayAge () {
+    const birthday = theOutput.value;
     var birthdate = new Date(document.getElementById("date").value);
     let today = new Date();
     var result = today.getFullYear() - birthdate.getFullYear();
-
-    document.getElementById("result").innerHTML = "Your age is " + result + " years old";
+    if ( birthday === "" ){
+     document.getElementById("result").innerHTML = `Please enter your birthday`
+    } else {
+        document.getElementById("result").innerHTML = "Your age is " + result + " years old";
+    }
 }
+
